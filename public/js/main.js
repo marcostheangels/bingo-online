14d07866a50202740dec556d78cfe1bb08ea924e
marcostheangels <116683889+marcostheangels@users.noreply.github.com>
2026-01-23 10:48:12 -0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
-
+// ✅ Variáveis globais
+let socket = null;
+let isAdminMode = false;
+let currentRoom = '';
+let cardType = '';
+let playerCards = [];
+let roomsDrawnNumbers = [];
+let gameEnded = false;
+let playerName = '';
+let currentStage = 'linha1';
+// ✅ CONEXÃO
+const SOCKET_URL = 'https://bingo-online-production.up.railway.app'; // ✅ removidos espaços
+socket = io(SOCKET_URL, {
+transports: ['websocket'],
+reconnection: true,
+reconnectionAttempts: Infinity
+});
